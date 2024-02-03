259a681292d074758fd4c902bd596e9b134d2d86
JusJae <42322548+JusJae@users.noreply.github.com>
2024-02-03 15:48:07 +0000

    feat: add users app and user-related files and views

---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import UserProfile
+from .forms import UserProfileForm
+
+
+@login_required
+def user_profile(request):
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_view')  # Redirect to the profile view
+    else:
+        form = UserProfileForm(instance=profile)
+    return render(request, 'users/user_profile.html', {'form': form})
