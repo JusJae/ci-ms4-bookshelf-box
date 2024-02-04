1ce32b3f05ab1ef0529b5f164e11bc8582019e29
JusJae <42322548+JusJae@users.noreply.github.com>
2024-02-04 03:03:48 +0000

    feat: update display_books.html and urls.py

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.subscription_view, name='subscriptions'),
-    path('display-books/', views.display_books, name='display_books')
+    path('display-books/<int:subscription_option_id>/', views.display_books, name='display_books')
 ]
