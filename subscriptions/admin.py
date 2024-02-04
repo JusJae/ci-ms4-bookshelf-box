11274bf50048ae9157cbf646eda7a87eadd18e3c
JusJae <42322548+JusJae@users.noreply.github.com>
2024-02-04 01:09:55 +0000

    feat: add display_selected_books method to SubscriptionOptionAdmin

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -3,11 +3,15 @@ from .models import SubscriptionOption
 
 
 class SubscriptionOptionAdmin(admin.ModelAdmin):
-    list_display = ('category', 'number_of_books', 'subscription_type', 'display_calculated_price')  # noqa
+    list_display = ('category', 'number_of_books', 'subscription_type', 'display_calculated_price', 'get_random_books')  # noqa
 
     def display_calculated_price(self, obj):
         return obj.calculate_price()
     display_calculated_price.short_description = 'Calculated Price'
+
+    def display_selected_books(self, obj):
+        return obj.random_books()
+    display_selected_books.short_description = 'Selected Books'
 
     list_filter = ('category', 'subscription_type')
     search_fields = ('category', 'subscription_type')
