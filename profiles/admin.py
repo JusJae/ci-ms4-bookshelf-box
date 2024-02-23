1c8ef79aca6573b19918ebd7ebf421e660021af4
JusJae <42322548+JusJae@users.noreply.github.com>
2024-02-23 00:45:33 +0000

    feat: add user profiles functionality and template and admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_country',
+                    'default_town_or_city', 'has_active_subscription')
+    search_fields = ('user__username', 'default_town_or_city',
+                     'default_country__name')
+    list_filter = ('has_active_subscription', 'default_country',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
