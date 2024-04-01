75989b6ba05e7d8d596922a442aaebff527e3345
JusJae <42322548+JusJae@users.noreply.github.com>
2024-04-01 22:41:03 +0000

    feat: add stripe card payment elent and js

--- checkout/views.py
+++ checkout/views.py
@@ -8,18 +8,19 @@ from .forms import OrderForm
 
 
 def checkout(request):
+
     box = request.session.get('box', {})
     print("Debug - Box contents:", box)
     if not box:
         messages.error(request, "There's nothing in your box at the moment")
-    # return redirect(reverse('subscriptions'))
+        return redirect(reverse('subscriptions'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        # 'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
-        # 'client_secret': intent.client_secret,
+        'stripe_public_key': 'pk_test_51OkDrNG6iutubJZORLX4eoy3QrTtVt6mUJMWl1GS6eWuxcMXHgxFnweYByNsmrOwJtO4aVXNKkJAOyhBUJBCsdY900aSP3sqdc',
+        'client_secret': 'test client secret',  # will need to change to actual client secret
     }
-    
+
     return render(request, template, context)
