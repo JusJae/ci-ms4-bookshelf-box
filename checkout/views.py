c71688e74cb35fc086b0404e407db2138af0473b
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-04-10 02:16:17 +0100

    feat: add checkout cache data

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +10,25 @@ from subscriptions.models import UserSubscriptionOption
 from boxes.contexts import box_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        json_data = json.loads(request.body.decode('utf-8'))
+        pid = json_data.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'box': json.dumps(request.session.get('box', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -39,7 +60,13 @@ def checkout(request):
         order_form = OrderForm(form_data)
 
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.stripe_pid = pid
+            order.original_box = json.dumps(box)
+            order.save()
+            for 
             try:
                 for subscription_id in box:
                     subscription = UserSubscriptionOption.objects.get(
@@ -58,7 +85,8 @@ def checkout(request):
                 return redirect(reverse('subscriptions'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
