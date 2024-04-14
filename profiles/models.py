40d1f42f2d78e33888b11cee14abd28320115611
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-04-14 11:19:53 +0100

    refactor: create_or_update_user_profile function signature in models.py

--- profiles/models.py
+++ profiles/models.py
@@ -34,7 +34,7 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(user, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     """ Create or update the user profile and create a stripe customer."""
     if created:
         user_profile = UserProfile.objects.create(user=instance)
