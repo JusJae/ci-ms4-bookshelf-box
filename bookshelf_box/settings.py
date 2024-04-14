35c0f099373cce0d7631ed706e46782963cc2ea0
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-04-14 11:33:18 +0100

    refactor: SECRET_KEY handling in settings.py

--- bookshelf_box/settings.py
+++ bookshelf_box/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rt&6-5x^!tizf#cud#x1$xz@(!*bwa#=3j^wwr9q#v4y)w$m*)'  # noqa: E501
-# SECRET_KEY = os.getenv('SECRET_KEY')
-# if not SECRET_KEY:
-#     raise ValueError("The SECRET_KEY environment variable is not set")
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("The SECRET_KEY environment variable is not set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
