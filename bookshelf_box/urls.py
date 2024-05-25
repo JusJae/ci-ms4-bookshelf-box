15674fbdc0c3c33098809c01ba406293933f9b90
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-05-25 23:28:12 +0100

    fix: add static and media file serving in urls.py

--- bookshelf_box/urls.py
+++ bookshelf_box/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('boxes/', include('boxes.urls')),
     path('checkout/', include('checkout.urls')),
     path('profiles/', include('profiles.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
