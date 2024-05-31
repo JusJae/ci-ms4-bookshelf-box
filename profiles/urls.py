22790fc344ba594a4683f76869f9fc22fd0db196
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-05-31 04:46:18 +0100

    fix: removal of duplicate url path and update cancel url path

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('update_subscription/<int:subscription_id>/', views.update_subscription, name='update_subscription'),
-    path('delete_subscription/<int:subscription_id>/', views.cancel_subscription, name='cancel_subscription'),
+    # path('update_subscription/<int:subscription_id>/', views.update_subscription, name='update_subscription'),
+    path('cancel_subscription/<int:subscription_id>/', views.cancel_subscription, name='cancel_subscription'),
 ]
