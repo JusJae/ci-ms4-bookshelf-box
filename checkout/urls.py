103bebcb113b8f066259204062c1f6c394c325bb
Jus Jae <42322548+JusJae@users.noreply.github.com>
2024-06-09 13:04:22 +0100

    refactor: fix indentation issue in checkout/urls.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
      path('cache_checkout_data/', views.cache_checkout_data,
           name='cache_checkout_data'),
      path('wh/', webhook, name='webhook'),
-     path('books/', include(('books.urls', 'books')),
-          namespace='books'),
+     path('books/', include(('books.urls', 'books'),
+                           namespace='books')),
+
 ]
