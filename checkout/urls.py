087dc83604fdebc42766eb2b269ef68a107ef911
Janelle Philips <42322548+JusJae@users.noreply.github.com>
2024-09-18 03:27:27 +0000

    refactor: update index and navbar html and checkout urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
      path('cache_checkout_data/', views.cache_checkout_data,
           name='cache_checkout_data'),
      path('wh/', webhook, name='webhook'),
-     path('books/', include(('books.urls', 'books'),
-                           namespace='books')),
+     path('books/', include(('books.urls', 'books'), namespace='books')),
 
 ]
