f7a4c093fb5f6656526945f8dc7e2f7051c17bc0
Janelle Philips <42322548+JusJae@users.noreply.github.com>
2024-09-18 05:30:19 +0000

    feat: add custom 404 page view

--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'home/index.html')
+
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
