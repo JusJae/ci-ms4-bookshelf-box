193915beb352133f11f6de3df0f55c6caf377a63
Janelle Philips <42322548+JusJae@users.noreply.github.com>
2024-09-19 10:08:46 +0000

    feat: update 404 and add 500 logic for error handling

--- bookshelf_box/urls.py
+++ bookshelf_box/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+from home import views as home_views
+
+# Custom error handling
+handler404 = home_views.custom_404
+handler500 = home_views.custom_500
 
 
 urlpatterns = [
@@ -30,7 +36,6 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
 ]
 
-handler404 = 'home.views.custom_404'
-
+# Serve media and static files during development
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
