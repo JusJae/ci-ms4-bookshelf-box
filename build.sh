dae43965db66669d8721861a2220010d3a7d7649
Jus Jae <42322548+JusJae@users.noreply.github.com>
2026-04-13 01:53:03 +0100

    fix: update build script to import books dataset before loading subscription options

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py import_books books/fixtures/ms4_books_dataset_170.csv
 python manage.py load_subscription_options subscriptions/fixtures/subscription_options.json
-gunicorn bookshelf_box.wsgi:application
